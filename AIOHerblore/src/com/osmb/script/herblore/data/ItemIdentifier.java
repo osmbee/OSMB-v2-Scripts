2445648ad5ac8ccc5c748cbb271fd7ce56a78b7a
Joe <73173735+osmbee@users.noreply.github.com>
2025-03-05 04:49:26 +0000

    Delete AIOHerblore directory

--- AIOHerblore/src/com/osmb/script/herblore/data/ItemIdentifier.java
+++
@@ -1,5 +0,0 @@
-package com.osmb.script.herblore.data;
-
-public interface ItemIdentifier {
-    int getItemID();
-}
--- AIOHerblore/src/com/osmb/script/herblore/data/Potion.java
+++
@@ -1,5 +0,0 @@
-package com.osmb.script.herblore.data;
-
-public interface Potion extends ItemIdentifier, Mixable{
-
-}
